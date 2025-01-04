9a5fa44d73c7ffdaf7e289d5d01218d9581f312e
alcomposer <alcomposer@users.noreply.github.com>
2025-01-04 12:39:51 +1030

    Refactor graph into it's own translation unit

---
+++ Nodes/AllNodes.h
@@ -0,0 +1,14 @@
+//
+// Created by alexw on 4/01/2025.
+//
+
+#include "../Nodes/AudioNodes.h"
+
+#include "../Nodes/SineWaveNode.h"
+#include "../Nodes/AddNode.h"
+#include "../Nodes/ValueNode.h"
+#include "../Nodes/MetroNode.h"
+#include "../Nodes/EnvelopeNode.h"
+#include "../Nodes/LFONode.h"
+#include "../Nodes/VolumeNode.h"
+#include "../Nodes/AudioOut.h"
