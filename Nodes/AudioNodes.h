f26622d2166231be5fe4dc752d9fbbfed6c6ee55
alcomposer <alcomposer@users.noreply.github.com>
2025-01-07 10:25:00 +1030

    Add license to files

--- Nodes/AudioNodes.h
+++ Nodes/AudioNodes.h
@@ -1,3 +1,9 @@
+/*
+// Copyright (c) 2024-2025 Alex Mitchell
+// For information on usage and redistribution, and for a DISCLAIMER OF ALL
+// WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+*/
+
 #include <vector>
 #include "../AudioPort.h"
 
