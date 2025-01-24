700dce7ca0bd6c384978f09e0236cc960f3136cc
alcomposer <alcomposer@users.noreply.github.com>
2025-01-24 17:49:07 +1030

    Use orange for audio port color

--- UI/Port.h
+++ UI/Port.h
@@ -39,9 +39,9 @@ public:
         nvgCircle(nvg, x + 5, y + 5, 5);
         //https://colorkit.co/color/1c4977/
         //nvgRGB(119, 28, 118)
-        auto pink = nvgRGB(92, 28, 119);
+        auto orange = nvgRGB(120, 74, 28);
         auto blue =  nvgRGB(28, 73, 119);
-        auto portCol = portNum == 0 ? blue : pink;
+        auto portCol = portNum == 0 ? blue : orange;
         nvgFillColor(nvg, portCol); // Blue fill for ports
         nvgFill(nvg);
 
