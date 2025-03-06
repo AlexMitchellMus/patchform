d76e3aa93459c65d2ed5d1229333c48c6cb8839e
alcomposer <alcomposer@users.noreply.github.com>
2025-03-06 11:22:03 +1030

    Use a shared data atom pool instead of per event atoms

--- Graph/Event.h
+++ Graph/Event.h
@@ -30,15 +30,15 @@ public:
     // Pointer to the head of the linked list of data atoms.
     DataAtom* data;
 
-    Event() : data(nullptr), tail(nullptr), usedAtomCount(0)
+    std::function<void(float)> addAtom = [](float) {};
+
+    Event() : data(nullptr), tail(nullptr)
     {
-        reserveAtoms(1024);
     }
 
     explicit Event(uint64_t timeStamp)
-        : timeStamp(timeStamp), data(nullptr), tail(nullptr), usedAtomCount(0)
+        : timeStamp(timeStamp), data(nullptr), tail(nullptr)
     {
-        reserveAtoms(1024);
     }
 
     void setTag(const std::string& tagName)
@@ -73,43 +73,9 @@ public:
         return nullptr;
     }
 
-    // Reserve a fixed capacity for the atom pool.
-    // This should be done once (or when you know you'll add more atoms) to avoid runtime allocation.
-    void reserveAtoms(size_t capacity)
+    int getNumAtoms() const
     {
-        atomPool.resize(capacity);
-        resetAtoms();
-    }
-
-    // Adds an atom to the end of the linked list.
-    // Uses a preallocated atom from the atomPool and connects it.
-    void addAtom(float value)
-    {
-        if (usedAtomCount >= atomPool.size())
-        {
-            throw std::runtime_error("Atom pool exhausted.");
-        }
-        // Get the next available atom.
-        DataAtom* newAtom = &atomPool[usedAtomCount++];
-        newAtom->atom = value;
-        newAtom->next = nullptr;
-
-        if (data == nullptr)
-        {
-            // First atom being added.
-            data = newAtom;
-            tail = newAtom;
-        }
-        else
-        {
-            tail->next = newAtom;
-            tail = newAtom;
-        }
-    }
-
-    int getNumAtoms()
-    {
-        return usedAtomCount;
+        return numAtoms;
     }
 
     uint64_t getTimeStamp() const
@@ -127,14 +93,15 @@ public:
     // The atomPool remains allocated, so the DataAtoms can be reused.
     void resetAtoms()
     {
-        usedAtomCount = 0;
         data = nullptr;
         tail = nullptr;
         timeStamp = 0;
+        numAtoms = 0;
     }
 
-private:
-    std::vector<DataAtom> atomPool;
-    size_t usedAtomCount;  // Number of atoms currently used in the pool.
     DataAtom* tail;        // Pointer to the last DataAtom in the linked list.
+
+    int numAtoms = 0;
+
+private:
 };
--- Graph/NodeContext.h
+++ Graph/NodeContext.h
@@ -11,8 +11,9 @@
 
 class EventPool {
 public:
-    EventPool(std::size_t initialSize = 1024) {
-        events.reserve(initialSize);
+    EventPool(std::size_t initialSize = 1024)
+    {
+        growAtomPool(initialSize * 100);
         growPool(initialSize);
     }
 
@@ -40,6 +41,7 @@ public:
     void releaseAllEvents()
     {
         freeStack = templateStack;
+        nextAtomIndex = 0;
     }
 
     // Grow the pool by adding N new events (call this OUTSIDE audio callback)
@@ -48,7 +50,36 @@ public:
         auto oldSize = events.size();
 
         auto newSize = oldSize + count;
+
         events.resize(newSize);
+
+        for (size_t i = oldSize; i < newSize; ++i)
+        {
+            events[i].addAtom = [this, i](float value) {
+                if (nextAtomIndex >= sharedAtomPool.size()) {
+                    throw std::runtime_error("Atom pool exhausted.");
+                }
+                // Get the next available atom.
+                DataAtom* newAtom = &sharedAtomPool[nextAtomIndex++];
+                newAtom->atom = value;
+                newAtom->next = nullptr;
+
+                // Retrieve the event using its index.
+                auto& evnt = events[i];
+
+                if (evnt.data == nullptr) {
+                    // First atom being added.
+                    evnt.data = newAtom;
+                    evnt.tail = newAtom;
+                    evnt.numAtoms = 1;
+                }
+                else {
+                    evnt.tail->next = newAtom;
+                    evnt.tail = newAtom;
+                    ++(evnt.numAtoms);
+                }
+            };
+        }
 
         freeStack.resize(newSize);
         std::iota(freeStack.begin(), freeStack.end(), 0);
@@ -57,10 +88,18 @@ public:
         std::iota(templateStack.begin(), templateStack.end(), 0);
     }
 
+    void growAtomPool(std::size_t count)
+    {
+        sharedAtomPool.resize(count);
+    }
+
 private:
-    std::vector<Event> events;               // Actual storage
-    std::vector<std::size_t> freeStack;      // Indices of free events
-    std::vector<std::size_t> templateStack;  // Template of a full freestack
+    std::vector<Event> events;                    // Actual storage
+    std::vector<std::size_t> freeStack;           // Indices of free events
+    std::vector<std::size_t> templateStack;       // Template of a full freestack
+
+    std::vector<DataAtom> sharedAtomPool;         // Shared data pool for all atoms
+    size_t nextAtomIndex = 0;
 };
 
 class NodeContext {
